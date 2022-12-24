74b8b84acd1691671b5987341ba1b6fa2d1fec77
drilon305 <drilonsulejmani305@gmail.com>
2022-12-24 22:59:48 +0100

    finishing navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
         "gray-50": "#EFE6E6",
         "gray-100": "#DFCCCC",
         "gray-500": "#5E0000",
-        "primary-100": "#FFE11E0",
+        "primary-100": "#FFE1E0",
         "primary-300": "#FFA6A3",
         "primary-500": "#FF6B66",
         "secondary-400": "#FFCD58",
