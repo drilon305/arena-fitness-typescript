15d9976233e65acc502de427b54299914894b488
drilon305 <drilonsulejmani305@gmail.com>
2023-01-02 22:54:50 +0100

    added framer motion for benefits page

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -2,6 +2,7 @@ import HText from "@/shared/HText";
 import { BenefitType, SelectedPage } from "@/shared/types";
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid"
 import { motion } from "framer-motion";
+import Benefit from "./Benefit";
 
 const benefits: Array<BenefitType> = [
   {
@@ -21,6 +22,14 @@ const benefits: Array<BenefitType> = [
     },
 ]
 
+const container = {
+  hiden: {},
+  visible: {
+    transition: { staggerChildren: 0.2}
+  },
+
+}
+
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -30,24 +39,48 @@ const Benefits = ({ setSelectedPage }: Props) => {
   return (
     <section
     id="benefits"
-    className="mx-auto min-h-full w-5/6 py-20"
+    className="mx-auto  min-h-full w-5/6 py-20"
     >
         <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
         >
                {/* HEADER */}
-            <div className="md:my-5 md:w-3/5">
+            <motion.div
+             className="md:my-5 md:w-3/5"
+             initial='hidden'
+             whileInView='visible'
+             viewport={{ once: true, amount: 0.5 }}
+             transition={{ duration: 0.5 }}
+             variants={{
+                hidden: { opacity: 0, x:-50},
+                visible: { opacity: 1, x: 0 }
+             }}
+            >
                 <HText>MORE THAN JUST A GYM</HText>
                 <p className="my-5 text-sm">We provide world class fitness equipment, trainers and classes to
             get you to your ultimate fitness goals with ease. We provide true
             care into each and every member.</p>
-            </div>
+            </motion.div>
 
               {/* BENEFITS */}
-          <div className="mt-5 items-center justify-between gap-8 md:flex"></div>
-      {benefits.map((benefit) => (
-        <Benefit />
+          <motion.div
+           className="mt-5 items-center justify-between gap-8 md:flex"
+           initial="hidden"
+           whileInView="visible"
+           viewport={{ once: true, amount: 0.5}}
+           variants={container}
+
+           >
+      {benefits.map((benefit: BenefitType) => (
+        <Benefit
+        key={benefit.title}
+        icon={benefit.icon}
+        title={benefit.title}
+        description={benefit.description}
+        setSelectedPage={setSelectedPage}
+        />
       ))}
+      </motion.div>
         </motion.div>
     </section>
   )
