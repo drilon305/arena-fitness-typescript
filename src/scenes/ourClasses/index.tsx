1e4e7b3c2252b486c2eb9366e2fd6657f698f83d
drilon305 <drilonsulejmani305@gmail.com>
2023-01-23 19:58:47 +0100

    fix small bug

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,4 +1,4 @@
-import { SelectedPage } from "@/shared/types"
+import { SelectedPage, ClassType } from "@/shared/types"
 import image1 from "@/assets/image1.png"
 import image2 from "@/assets/image2.png"
 import image3 from "@/assets/image3.png"
@@ -8,7 +8,6 @@ import image6 from "@/assets/image6.png"
 import { motion } from "framer-motion"
 import HText from "@/shared/HText"
 import Class from "./Class"
-import { ClassType } from "react"
 
 
 
